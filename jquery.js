019acc56c571fb545a5f115f42fb03cdf6339f11
Joseph Kingsley <kingsleyj809@gmail.com>
2023-01-22 14:46:13 +0100

    added the jquery

--- jquery.js
+++ jquery.js
@@ -1,13 +1,13 @@
-// $(()=>{
+$(()=>{
 
-//     $(".services h1").hide();
-//     $(".see-text").hide();
-//    $(window).scroll(()=>{
+    $(".services h1").hide();
+    $(".see-text").hide();
+   $(window).scroll(()=>{
        
-//     $(".services h1").fadeIn(8000);
-//    });
+    $(".services h1").fadeIn(8000);
+   });
 
 
-//     $(".see-text").slideDown(2000);
+    $(".see-text").slideDown(2000);
     
-// });
+});
