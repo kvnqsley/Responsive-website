0e98c1fe5b9fd53d5b385f7fd252d0d34cf36e6e
Joseph Kingsley <kingsleyj809@gmail.com>
2023-01-22 14:41:06 +0100

    compiled it into modular javascript

--- app.js
+++ app.js
@@ -1,51 +1,117 @@
-var mobileMenu = document.querySelector('.navbar-toggle')
-var navBarMenu = document.querySelector('.navbar-menu');
-var navBarLinks = document.querySelectorAll('.navbar-links')
-var mobileMenuContainer= document.getElementById('mobile-menu')
-var xIcon = document.getElementsByClassName('x-icon')
-var bar =document.querySelectorAll('.bar')
-ul=document.querySelector('.navbar-item .sub1')
-item=document.querySelectorAll('.navbar-item')
-const servicesH1= document.querySelector('.services h1');
-const marquee = document.getElementById('weather');
+// var mobileMenu = document.querySelector('.navbar-toggle')
+// var navBarMenu = document.querySelector('.navbar-menu');
+// var navBarLinks = document.querySelectorAll('.navbar-links')
+// var mobileMenuContainer= document.getElementById('mobile-menu')
+// var xIcon = document.getElementsByClassName('x-icon')
+// var bar =document.querySelectorAll('.bar')
+// ul=document.querySelector('.navbar-item .sub1')
+// item=document.querySelectorAll('.navbar-item')
+// const servicesH1= document.querySelector('.services h1');
+// const marquee = document.getElementById('weather');
 
-const close =document.createElement('button');
-mobileMenuContainer.appendChild(close);
-const x =document.createTextNode('X');
-closeBtn=close.appendChild(x);
-close.setAttribute('class','close-btn')
+// const close =document.createElement('button');
 
-mobileMenu.addEventListener('click',(e)=>{
-    navBarMenu.classList.toggle('display-nav');
-    navBarMenu.classList.add('transition');
+(function(){
+   
+    const app={
+    
+        // initialization
+    init:()=>{
+    app.cacheDom();
+app._render();
+app.bindEvent();
+app.displaynav();
+    },
+   //cacheDom
+    cacheDom:()=>{
+        this.mobileMenu= document.querySelector('.navbar-toggle');
+        this.navBarMenu= document.querySelector('.navbar-menu');
+        this.navBarLinks= document.querySelectorAll('.navbar-links');
+        this.mobileMenuContainer=document.getElementById('mobile-menu');
+        this.xIcon= document.getElementsByClassName('x-icon')
+        this.bar =document.querySelectorAll('.bar')
+        this.ul=document.querySelector('.navbar-item .sub1')
+        this.item=document.querySelectorAll('.navbar-item')
+        this.servicesH1= document.querySelector('.services h1');
+    this.marquee = document.getElementById('weather');
+    this.close =document.createElement('button');
+    this.sub1=document.querySelector("ul.sub1");
+    this.sub2=document.querySelector("ul.sub2");
+
+     this.extraUl=document.querySelector('#extra ul');
+     this.extra=document.querySelector('.about')
+     this.mobileMenuContainer.appendChild(close);
+this.x =document.createTextNode('X');
+this.closeBtn=close.appendChild(x);
+this.close.setAttribute('class','close-btn')
+    },
+
+    bindEvent:()=>{
+this.mobileMenu.addEventListener('click',app.displaynav.bind(this)
+    );
+
+
+
+
+    },
+    _render:()=>{
+        if (window.innerWidth<=600) {
+           this.navBarLinks.forEach(element => {
+                element.style.transform='translateX(-40%)';
+            
+    })
+}
+    },
+    displaynav:()=>{
+        this.navBarMenu.classList.toggle('display-nav');
+        this.navBarMenu.classList.add('transition');
+        this.bar.forEach(element => {
+            element.classList.toggle("inactive")
+            });
+            this.close.classList.toggle('active')
+    
+    }
+    
+}
+app.init()
+})();
+
+// mobileMenuContainer.appendChild(close);
+// const x =document.createTextNode('X');
+// closeBtn=close.appendChild(x);
+// close.setAttribute('class','close-btn')
+
+// mobileMenu.addEventListener('click',(e)=>{
+//     navBarMenu.classList.toggle('display-nav');
+//     navBarMenu.classList.add('transition');
  
-bar.forEach(element => {
-    element.classList.toggle("inactive")
-    });
-    close.classList.toggle('active')
+// bar.forEach(element => {
+//     element.classList.toggle("inactive")
+//     });
+//     close.classList.toggle('active')
 
-})
+// })
 
-var sub1=document.querySelector("ul.sub1");
-sub2=document.querySelector("ul.sub2");
+// var sub1=document.querySelector("ul.sub1");
+// sub2=document.querySelector("ul.sub2");
 
-     extraUl=document.querySelector('#extra ul');
-     extra=document.querySelector('.about')
+//      extraUl=document.querySelector('#extra ul');
+//      extra=document.querySelector('.about')
 
 
-function displayOff(e) {
-    if (window.innerWidth<=600) {
-    navBarLinks.forEach(element => {
-        element.style.transform='translateX(-40%)';
-        //element.previousElementSibling.style.display="none"
+// function displayOff(e) {
+//     if (window.innerWidth<=600) {
+//     navBarLinks.forEach(element => {
+//         element.style.transform='translateX(-40%)';
+//         //element.previousElementSibling.style.display="none"
         
-    });
+//     });
  
-    item.forEach(element => {
-       console.log();
-    });
-}
-}
+//     item.forEach(element => {
+//        console.log();
+//     });
+// }
+// }
 function display2() {
 
     sub2.classList.remove('display')
@@ -127,3 +193,4 @@ const params ={
     
 // }
 // console.log(myPromise());
+
